6a62162f76b89bb947b065d576e8bebf459e6c90
binarysailor <binarysailing@gmail.com>
2013-05-23 21:42:28 +0200

    product search

---
+++ shopping/src/net/binarysailor/shopping/catalog/FilteredCategory.java
@@ -0,0 +1,59 @@
+package net.binarysailor.shopping.catalog;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import net.binarysailor.shopping.catalog.model.Category;
+import net.binarysailor.shopping.catalog.model.Product;
+
+class FilteredCategory {
+
+	private Category category;
+	private List<Product> products;
+	private String filterText;
+
+	public static FilteredCategory create(Category category, String filterText) {
+		FilteredCategory fc = new FilteredCategory(category, filterText);
+		for (Product product : category.getProducts()) {
+			fc.addProductIfMatches(product);
+		}
+		return fc.isEmpty() ? null : fc;
+	}
+
+	private FilteredCategory(Category category, String filterText) {
+		this.category = category;
+		this.products = new LinkedList<Product>();
+		this.filterText = filterText;
+		if (this.filterText != null) {
+			this.filterText = this.filterText.toLowerCase();
+		}
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public List<Product> getProducts() {
+		return products;
+	}
+
+	public boolean isEmpty() {
+		return products.isEmpty();
+	}
+
+	private boolean addProductIfMatches(Product product) {
+		if (!filteringRequested() || matches(product)) {
+			products.add(product);
+			return true;
+		}
+		return false;
+	}
+
+	private boolean matches(Product product) {
+		return product.getName().toLowerCase().contains(filterText);
+	}
+
+	private boolean filteringRequested() {
+		return filterText != null && !filterText.trim().equals("");
+	}
+}

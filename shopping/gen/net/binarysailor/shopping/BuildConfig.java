3f2dfa776dab21459af21a2a4c927a57d57d7d3b
tszymanski <tszymanski@bravurasolutions.com>
2014-10-17 17:19:14 +0200

    deleted the old source subfolder

--- shopping/gen/net/binarysailor/shopping/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package net.binarysailor.shopping;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- shopping/src/net/binarysailor/shopping/common/DatabaseOperations.java
+++
@@ -1,7 +0,0 @@
-package net.binarysailor.shopping.common;
-
-import android.database.sqlite.SQLiteDatabase;
-
-public interface DatabaseOperations {
-	Object execute(SQLiteDatabase db);
-}
